6a23f6fa398b5ad6303182320d14c1ba83f1c200
Daniel <daniel.barczynski@onet.pl>
2022-01-21 14:37:20 +0100

    layout

---
+++ readit/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace readit.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
