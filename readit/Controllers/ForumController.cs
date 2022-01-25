af9acc7baf3a6b244ceec85787ff07f0195c6e7d
Daniel <daniel.barczynski@onet.pl>
2022-01-25 16:58:06 +0100

    crud before -ignorechanges

--- readit/Controllers/ForumController.cs
+++ readit/Controllers/ForumController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using readit.Database;
+using readit.Models;
 
 namespace readit.Controllers
 {
@@ -15,5 +16,41 @@ namespace readit.Controllers
         {
             return View(await _appDbContext.Forums.ToListAsync());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ForumModel forumModel)
+        {
+            _appDbContext.Forums.Add(forumModel);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            //ForumModel forumModel = new ForumModel();
+           
+            var forumModel = await _appDbContext.Forums.FirstOrDefaultAsync(m => m.Id == id);
+            await _appDbContext.Forums.FindAsync(id);
+
+            //if (id == null || forumModel == null)
+            //{
+            //    return NotFound();
+            //}
+
+            return View(forumModel);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(ForumModel forumModel)
+        //{
+        //    _appDbContext.Forums.Update(forumModel);
+        //    await _appDbContext.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
