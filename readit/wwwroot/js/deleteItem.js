bcf44f2ddef476cb18b6284109ce65d2c5f37a3e
Daniel <daniel.barczynski@onet.pl>
2022-02-11 13:58:46 +0100

    js delete fixed

--- readit/wwwroot/js/deleteItem.js
+++ readit/wwwroot/js/deleteItem.js
@@ -1,26 +1,22 @@
 ﻿'use strict';
 
-const btnDelete = document.querySelector('aw');
+const btnDelete = document.querySelectorAll('.del');
 const confirmWindow = document.querySelector('.confirm');
 const overlay = document.querySelector('.overlay');
 const btnNo = document.getElementById('no');
 
-//var el = document.querySelectorAll(".the_class")
-//for (var i = 0, ilen = el.length - 1; i < ilen; i++) {
-//    el[i].className = "a_new_class"
-//}
-
 const showConfirm = function () {
-    console.log('hi');
     confirmWindow.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
+
+for (let i = 0; i < btnDelete.length; i++) {
+    btnDelete[i].addEventListener('click', showConfirm);
+}
 
 const closeConfirm = function () {
     confirmWindow.classList.add('hidden');
     overlay.classList.add('hidden');
 };
 
-btnDelete.addEventListener('click', showConfirm);
 btnNo.addEventListener('click', closeConfirm);
-console.log('hi');
